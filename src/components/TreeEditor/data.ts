fc7b110f0b53e449828bcdce4788c30ae631e89d
VegetableBirddd <wu13540666210@163.com>
2024-01-19 16:13:42 +0800

    优化：树形组件勾选

--- src/components/TreeEditor/data.ts
+++ src/components/TreeEditor/data.ts
@@ -1,7 +1,8 @@
 import type { TreeDataNode } from 'antd';
 
-interface MyTreeDataNode extends TreeDataNode {
-  parentNode: null | string;
+export interface MyTreeDataNode extends TreeDataNode {
+  checked?: boolean; //是否打√了
+  parentKey: null | string;
   children?: MyTreeDataNode[]; //覆盖以前的children
 }
 
@@ -9,413 +10,419 @@ export const treeData:MyTreeDataNode[] = [
   {
     title: 'title',
     key: 'title',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children: [
       {
         title: 'id',
         key: 'title-id',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'show',
         key: 'title-show',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'text',
         key: 'title-text',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'link',
         key: 'title-link',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'target',
         key: 'title-target',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'textStyle',
         key: 'title-textStyle',
-        parentNode:'title',
+        parentKey:'title',
         children:[
             {
                 title: 'color',
                 key: 'title-textStyle-color',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'fontStyle',
                 key: 'title-textStyle-fontStyle',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'fontWeight',
                 key: 'title-textStyle-fontWeight',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'fontFamily',
                 key: 'title-textStyle-fontFamily',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'fontSize',
                 key: 'title-textStyle-fontSize',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'lineHeight',
                 key: 'title-textStyle-lineHeight',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'width',
                 key: 'title-textStyle-width',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'height',
                 key: 'title-textStyle-height',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textBorderColor',
                 key: 'title-textStyle-textBorderColor',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textBorderWidth',
                 key: 'title-textStyle-textBorderWidth',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textBorderType',
                 key: 'title-textStyle-textBorderType',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textBorderDashOffset',
                 key: 'title-textStyle-textBorderDashOffset',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textShadowColor',
                 key: 'title-textStyle-textShadowColor',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textShadowBlur',
                 key: 'title-textStyle-textShadowBlur',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textShadowOffsetX',
                 key: 'title-textStyle-textShadowOffsetX',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'textShadowOffsetY',
                 key: 'title-textStyle-textShadowOffsetY',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'overflow',
                 key: 'title-textStyle-overflow',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'ellipsis',
                 key: 'title-textStyle-ellipsis',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
             {
                 title: 'rich',
                 key: 'title-textStyle-rich',
-                parentNode:'title-textStyle',
+                parentKey:'title-textStyle',
             },
         ]
       },
       {
         title: 'subtext',
         key: 'title-subtext',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'sublink',
         key: 'title-sublink',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'subtarget',
         key: 'title-subtarget',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'subtextStyle',
         key: 'title-subtextStyle',
-        parentNode:'title',
+        parentKey:'title',
         children:[
             {
                 title: 'color',
                 key: 'title-subtextStyle-color',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'fontStyle',
                 key: 'title-subtextStyle-fontStyle',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'fontWeight',
                 key: 'title-subtextStyle-fontWeight',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'fontFamily',
                 key: 'title-subtextStyle-fontFamily',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'fontSize',
                 key: 'title-subtextStyle-fontSize',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'align',
                 key: 'title-subtextStyle-align',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'verticalAlign',
                 key: 'title-subtextStyle-verticalAlign',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'lineHeight',
                 key: 'title-subtextStyle-lineHeight',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'width',
                 key: 'title-subtextStyle-width',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'height',
                 key: 'title-subtextStyle-height',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textBorderColor',
                 key: 'title-subtextStyle-textBorderColor',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textBorderWidth',
                 key: 'title-subtextStyle-textBorderWidth',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textBorderType',
                 key: 'title-subtextStyle-textBorderType',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textBorderDashOffset',
                 key: 'title-subtextStyle-textBorderDashOffset',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textShadowColor',
                 key: 'title-subtextStyle-textShadowColor',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textShadowBlur',
                 key: 'title-subtextStyle-textShadowBlur',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textShadowOffsetX',
                 key: 'title-subtextStyle-textShadowOffsetX',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'textShadowOffsetY',
                 key: 'title-subtextStyle-textShadowOffsetY',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'overflow',
                 key: 'title-subtextStyle-overflow',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'ellipsis',
                 key: 'title-subtextStyle-ellipsis',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
             {
                 title: 'rich',
                 key: 'title-subtextStyle-rich',
-                parentNode:'title-subtextStyle',
+                parentKey:'title-subtextStyle',
             },
         ]
       },
       {
         title: 'textAlign',
         key: 'title-textAlign',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'textVerticalAlign',
         key: 'title-textVerticalAlign',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'triggerEvent',
         key: 'title-triggerEvent',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'padding',
         key: 'title-padding',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'itemGap',
         key: 'title-itemGap',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'zlevel',
         key: 'title-zlevel',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'z',
         key: 'title-z',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'left',
         key: 'title-left',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'top',
         key: 'title-top',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'right',
         key: 'title-right',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'bottom',
         key: 'title-bottom',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'backgroundColor',
         key: 'title-backgroundColor',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'borderColor',
         key: 'title-borderColor',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'borderWidth',
         key: 'title-borderWidth',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'borderRadius',
         key: 'title-borderRadius',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'shadowBlur',
         key: 'title-shadowBlur',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'shadowColor',
         key: 'title-shadowColor',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'shadowOffsetX',
         key: 'title-shadowOffsetX',
-        parentNode:'title',
+        parentKey:'title',
       },
       {
         title: 'shadowOffsetY',
         key: 'title-shadowOffsetY',
-        parentNode:'title',
+        parentKey:'title',
       },
     ],
   },
   {
     title: 'legend',
     key: 'legend',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
-    children:[]
+    children:[
+        {
+            title: 'id',
+            key: 'legend-id',
+            parentKey:'legend',
+        },
+    ]
   },
   {
     title: 'grid',
     key: 'grid',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'xAxis',
     key: 'xAxis',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'yAxis',
     key: 'yAxis',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'tooltip',
     key: 'tooltip',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'toolbox',
     key: 'toolbox',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'dataset',
     key: 'dataset',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
   {
     title: 'series',
     key: 'series',
-    parentNode:null,
+    parentKey:null,
     checkable:false,
     children:[]
   },
--- src/components/TreeEditor/index.tsx
+++ src/components/TreeEditor/index.tsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 import { Tree } from 'antd';
-import { treeData } from './data';
+import { MyTreeDataNode, treeData } from './data';
+import { useImmer } from 'use-immer';
+function findByPos(data:MyTreeDataNode,pos:string){ // 通过位置寻找对象 '0-0-1'表示 data[0].children[0].children[1]
+  let arr = pos.split('-');// [0,0,1]
+  let index = 1;
+  let res = data[arr[index++]];
+  while(index<arr.length){
+    let num = arr[index++];
+    res = res.children[num];
+  }
+  return res;
+}
 
 const TreeEditor: React.FC = () => {
   const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
-  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>([]);
+  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>(["title-id"]);
   const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
   const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
-
+  const [storeData,updateData] = useImmer<any>(treeData);
   const onExpand = (expandedKeysValue: React.Key[]) => {
-    // console.log('onExpand', expandedKeysValue);
-    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-    // or, you can remove all expanded children keys.
     setExpandedKeys(expandedKeysValue);
     setAutoExpandParent(false);
   };
 
   const onCheck = (checkedKeysValue,info:any) => {
-    console.log('onCheck', checkedKeysValue);
-    console.log('e:',info)
+    let { checked,node:{ pos } } = info;
     setCheckedKeys(checkedKeysValue);
+    updateData(draft=>{
+      let dd = findByPos(draft,pos);
+      dd.checked = checked;
+    })
   };
 
   const onSelect = (selectedKeysValue: React.Key[], info: any) => {
-    // console.log('onSelect', info);
+    console.log('onSelect', info);
     setSelectedKeys(selectedKeysValue);
   };
 
@@ -37,7 +48,12 @@ const TreeEditor: React.FC = () => {
       checkedKeys={checkedKeys}
       onSelect={onSelect}
       selectedKeys={selectedKeys}
-      treeData={treeData}
+      treeData={storeData}
+      titleRender={(nodeData:any)=>{
+        return <div style={{height:nodeData.checked?50:20}}>
+          {nodeData.title}
+        </div>
+      }}
     />
   );
 };

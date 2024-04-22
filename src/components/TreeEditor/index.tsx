8c555b514eb19078e588184e61ac8b02effdf7f0
VegetableBirddd <wu13540666210@163.com>
2024-04-22 11:40:13 +0800

    update:修改样式

--- src/components/TreeEditor/index.tsx
+++ src/components/TreeEditor/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { CollapseProps, Tree,Collapse  } from 'antd';
+import { CollapseProps, theme ,Collapse  } from 'antd';
 import { MyTreeDataNode, treeData } from './data';
 import { useImmer } from 'use-immer';
+import { CaretRightOutlined } from '@ant-design/icons';
 function findByPos(data:MyTreeDataNode,pos:string){ // 通过位置寻找对象 '0-0-1'表示 data[0].children[0].children[1]
   let arr = pos.split('-');// [0,0,1]
   let index = 1;
@@ -12,76 +13,54 @@ function findByPos(data:MyTreeDataNode,pos:string){ // 通过位置寻找对象 
   }
   return res;
 }
-const text = '属性'
-const items: CollapseProps['items'] = [
-  {
-    key: '1',
-    label: 'This is panel header 1',
-    children: <p>{text}</p>,
-  },
-  {
-    key: '2',
-    label: 'This is panel header 2',
-    children: <p>{text}</p>,
-  },
-  {
-    key: '3',
-    label: 'This is panel header 3',
-    children: <p>{text}</p>,
-  },
-];
 
-function turnItems(data){
 
+function getItems(data,panelStyle){
+  const text = '属性'
+  const items: CollapseProps['items'] = [
+    {
+      key: '1',
+      label: 'This is panel header 1',
+      children: <p>{text}</p>,
+      style: panelStyle,
+    },
+    {
+      key: '2',
+      label: 'This is panel header 2',
+      children: <p>{text}</p>,
+      style: panelStyle,
+    },
+    {
+      key: '3',
+      label: 'This is panel header 3',
+      children: <p>{text}</p>,
+      style: panelStyle,
+    },
+  ];
+
+  return items;
 }
 
 const TreeEditor: React.FC = () => {
-  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
-  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>(["title-id"]);
-  const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
-  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
-  const [storeData,updateData] = useImmer<any>(treeData);
-  const onExpand = (expandedKeysValue: React.Key[]) => {
-    setExpandedKeys(expandedKeysValue);
-    setAutoExpandParent(false);
+  const { token } = theme.useToken();
+  const panelStyle: React.CSSProperties = {
+    marginBottom: 6,
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    // border: 'none',
   };
-
-  const onCheck = (checkedKeysValue,info:any) => {
-    let { checked,node:{ pos } } = info;
-    setCheckedKeys(checkedKeysValue);
-    updateData(draft=>{
-      let dd = findByPos(draft,pos);
-      dd.checked = checked;
-    })
-  };
-
-  const onSelect = (selectedKeysValue: React.Key[], info: any) => {
-    console.log('onSelect', info);
-    setSelectedKeys(selectedKeysValue);
-  };
-
   return (
     <>
       <div>
-        
+
       </div>
-      <Collapse accordion items={items} />
-      {/* <Tree
-        checkable
-        onExpand={onExpand}
-        expandedKeys={expandedKeys}
-        autoExpandParent={autoExpandParent}
-        onCheck={onCheck}
-        checkedKeys={checkedKeys}
-        onSelect={onSelect}
-        selectedKeys={selectedKeys}
-        treeData={storeData}
-        titleRender={(nodeData:any)=>{
-          return <div style={{height:nodeData.checked?50:20}}>
-            {nodeData.title}
-          </div>
-        }}
-      /> */}
+      <Collapse 
+        bordered={false}
+        accordion 
+        items={getItems(undefined,panelStyle)}
+        style={{ background: token.colorBgContainer }}
+        expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+      />
     </>
     
   );
